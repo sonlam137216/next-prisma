6ae0b7d6f72a069397dae560e4314c9ca3d866b9
sonlt <sonlam@gmail.com>
2025-05-30 22:59:52 +0700

    fix: load image vps

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'standalone',
   images: {
-    domains: ['localhost', 'theme.hstatic.net'],
+    domains: ['localhost', 'theme.hstatic.net', '14.225.212.72'],
   },
 }
 
