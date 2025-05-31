1ef8dab51a9ab8d8649b56cb65e875a48bfaab65
sonlt <sonlam@gmail.com>
2025-06-01 00:40:01 +0700

    fix: build step

--- app/about/chat-luong/page.tsx
+++ app/about/chat-luong/page.tsx
@@ -2,7 +2,6 @@
 
 import { Container } from "@/components/ui/container";
 import { motion } from "framer-motion";
-import Image from "next/image";
 
 export default function QualityPage() {
   return (
