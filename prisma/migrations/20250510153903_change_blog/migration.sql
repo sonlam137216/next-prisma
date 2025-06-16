3be1010bbf92a63478b634cc963f4115f69d5bb8
sonlam <lamtruongson137216@gmail.com>
2025-06-17 01:04:58 +0700

    fix: add migrations

--- prisma/migrations/20250510153903_change_blog/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "BlogPost" ADD COLUMN     "category" TEXT NOT NULL DEFAULT 'Uncategorized',
-ADD COLUMN     "description" TEXT,
-ADD COLUMN     "readingTime" INTEGER NOT NULL DEFAULT 5,
-ALTER COLUMN "path" DROP NOT NULL;
--- prisma/migrations/20250615093919_add_product_discount/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "discountEndDate" TIMESTAMP(3),
-ADD COLUMN     "discountPercentage" INTEGER,
-ADD COLUMN     "discountPrice" DOUBLE PRECISION,
-ADD COLUMN     "discountStartDate" TIMESTAMP(3),
-ADD COLUMN     "hasDiscount" BOOLEAN NOT NULL DEFAULT false;
